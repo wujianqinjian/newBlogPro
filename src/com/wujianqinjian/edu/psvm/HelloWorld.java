f0d77aa240701bd3d9e923b6a5b0cbb49ea29914
wujianqinjian <wujianqinjian@163.com>
2018-05-06 23:23:50 +0800

    first commit

---
+++ src/com/wujianqinjian/edu/psvm/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.wujianqinjian.edu.psvm;
+
+/**
+ * Created by JianH on 2018/5/6.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+
+    }
+}
